d883cdc113b725003ecff8147f4c92992ac467d3
Artem <vebulogmetra@yandex.ru>
2024-01-30 16:14:20 +0300

    add application entrypoint

---
+++ main.py
@@ -0,0 +1,11 @@
+from src.products import Product, get_products
+from src.categories import get_categories
+
+
+categories: tuple[str] = get_categories()
+products: list[Product] = get_products(limit=5, skip=0)
+
+
+print(categories)
+print("#" * 30)
+print(products)
