5c29245f08cf84571ef3ea21a5af2189419d9e60
Artem <vebulogmetra@yandex.ru>
2024-01-30 16:30:09 +0300

    Update readme

--- main.py
+++ main.py
@@ -4,8 +4,3 @@ from src.categories import get_categories
 
 categories: tuple[str] = get_categories()
 products: list[Product] = get_products(limit=5, skip=0)
-
-
-print(categories)
-print("#" * 30)
-print(products)
