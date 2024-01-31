67a567245e11de1d3762cfd9303d4c3bbfd51be8
Artem <vebulogmetra@yandex.ru>
2024-02-01 02:47:06 +0300

    update initdb.sh

--- src/scripts/initdb.sh
+++ src/scripts/initdb.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# Параметры подключения к базе данных SQLite
-DB_FILE="../../dammy-api.db"
-SQL_FILE="./create_tables.sql"
+# Получение полного пути к файлам
+BASEDIR=$(dirname "$0")
+DB_FILE="dummy-api.db"
+SQL_FILE="$BASEDIR/create_tables.sql"
+
+DB_FILE_PATH=$(realpath "$DB_FILE")
+SQL_FILE_PATH=$(realpath "$SQL_FILE")
+echo "[DB FILE PATH] = $DB_FILE_PATH"
+echo "[SQL_FILE_PATH] = $SQL_FILE_PATH"
 
 if [ ! -f "$DB_FILE" ]; then
-    echo "База данных $DB_FILE не существует. Создание новой базы данных..."
+    echo "[=>]База данных $DB_FILE не существует. Создание новой базы данных..."
     touch "$DB_FILE"
+    echo "[=>]Создание таблиц..."
     sqlite3 "$DB_FILE" < "$SQL_FILE"
-    echo "База данных и таблицы успешно созданы."
+    echo "[+]База данных и таблицы успешно созданы."
 else
-    echo "База данных $DB_FILE уже существует. Пропуск."
+    echo "[-]База данных $DB_FILE уже существует. Пропуск."
+    echo "[=>]Создание таблиц..."
+    sqlite3 "$DB_FILE" < "$SQL_FILE"
+    echo "[+]Таблицы успешно созданы."
 fi
