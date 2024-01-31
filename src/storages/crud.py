95acee71a3af61ee17ee0a8f8cceef49e76e650f
Artem <vebulogmetra@yandex.ru>
2024-02-01 02:46:18 +0300

    create SqliteCRUD in storage/crud

--- src/storages/crud.py
+++ src/storages/crud.py
@@ -1,68 +1,57 @@
 from src.DTOs import ProductDTO, CategoryDTO
-from src.storages.manager import DatabaseManager
-from typing import Iterable
+from sqlite3 import Connection
+import sqlite3
+from typing import TypeVar
+from contextlib import closing
 
 
-def insert_product(db_manager: DatabaseManager, product_data: ProductDTO):
-    query = """INSERT INTO products (product_id, title, description, price, thumbnail, category_id)
-               VALUES (:product_id, :title, :description, :price, :thumbnail, :category_id)
-               RETURNING product_id"""
-    result = db_manager.execute_query(query, product_data.to_dict())
-    return result[0]
+EntityDTO = TypeVar("EntityDTO", ProductDTO, CategoryDTO)
 
 
-def select_product(
-    db_manager: DatabaseManager, by_field: str, by_value: str | int | float
-):
-    query = """SELECT * FROM products WHERE :by_field = :by_value"""
-    values = {"by_field": by_field, "by_value": by_value}
-    result = db_manager.execute_query(query, values)
-    return result[0]
+class SqliteCRUD:
+    def __init__(self, db_file: str):
+        self.conn: Connection = sqlite3.connect(db_file)
 
+    def insert_entity(self, table_name: str, entity_data: EntityDTO):
+        _entity_data: dict = entity_data.to_dict()
+        columns = ", ".join(_entity_data.keys())
+        placeholders = ", ".join(":" + key for key in _entity_data.keys())
+        query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+        self._execute_query(query, _entity_data)
 
-def delete_product(
-    db_manager: DatabaseManager, by_field: str, by_value: str | int | float
-):
-    query = """DELETE FROM products WHERE :by_field = :by_value RETURNING product_id"""
-    values = {"by_field": by_field, "by_value": by_value}
-    result = db_manager.execute_query(query, values)
-    return result[0]
+    def get_entities(self, table_name: str):
+        query = f"SELECT * FROM {table_name}"
+        return self._execute_query(query)
 
+    def get_entity(self, table_name: str, entity_field: str, entity_value: int | str):
+        query = f"SELECT * FROM {table_name} WHERE {entity_field} = :entity_value"
+        return self._execute_query(query, {"entity_value": entity_value})
 
-def insert_category(db_manager: DatabaseManager, category_data: CategoryDTO):
-    query = """INSERT INTO categories (name) VALUES (:name) RETURNING category_id"""
-    values = {"name": category_data.name}
-    result = db_manager.execute_query(query, values)
-    return result[0]
+    def delete_entity(self, table_name: str, entity_id: int):
+        query = f"DELETE FROM {table_name} WHERE id = :entity_id"
+        self._execute_query(query, {"entity_id": entity_id})
 
+    def _execute_query(self, query, values=None):
+        with closing(self.conn.cursor()) as cursor:
+            try:
+                if values:
+                    cursor.execute(query, values)
+                else:
+                    cursor.execute(query)
 
-def select_category(
-    db_manager: DatabaseManager, by_field: str, by_value: str | int | float
-):
-    query = """SELECT * FROM categories WHERE :by_field = :by_value"""
-    values = {"by_field": by_field, "by_value": by_value}
-    result = db_manager.execute_query(query, values)
-    return result[0]
+                if query.strip().lower().startswith("select"):
+                    return cursor.fetchall()
 
+            except Exception as e:
+                print(f"[x] Sqlite execute query failed. Rollback...: {e}")
+                self.conn.rollback()
+                raise e
 
-def delete_category(
-    db_manager: DatabaseManager, by_field: str, by_value: str | int | float
-):
-    query = """DELETE FROM categories WHERE :by_field = :by_value RETURNING category_id"""
-    values = {"by_field": by_field, "by_value": by_value}
-    result = db_manager.execute_query(query, values)
-    return result[0]
-
-
-# postgres_manager = DatabaseManager(
-#     db_type=DB_TYPE_POSTGRES,
-#     database=POSTGRES_DB,
-#     user=POSTGRES_USER,
-#     password=POSTGRES_PASSWORD,
-#     host=POSTGRES_HOST,
-#     port=POSTGRES_PORT,
-# )
-# postgres_manager.connect()
-# result = postgres_manager.execute_query("SELECT * FROM products")
-# print(result)
-# postgres_manager.close()
+    def close_connection(self):
+        if self.conn:
+            try:
+                self.conn.commit()
+            except Exception as e:
+                print(f"[x] Sqlite commit failed: {e}")
+            finally:
+                self.conn.close()
